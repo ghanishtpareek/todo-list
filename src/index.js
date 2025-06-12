07bb41d7524c37d2c256813c7ad52fdef7812629
Ghanisht Pareek <79847165+ghanishtpareek@users.noreply.github.com>
2025-06-12 11:59:49 +0000

    add task class code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import Task from "./task.js"
 console.log("it is a todo list application")
+alert("hi in my app")
+
+const writing = new Task("writing","this is a writing task very important","low")
+console.log(writing)
+
+writing.priority = "medium"
+console.log(writing)
---
+++ src/task.js
@@ -0,0 +1,73 @@
+export default class Task {
+    #name = "New Task"
+    #description = "A new task"
+    #dueDate
+    #priority = "low"
+    static validPriorities = ["low", "medium", "high"]
+
+    constructor(name, description, priority, dueDate) {
+        alert("hello class task")
+
+        if (!dueDate) {
+            //if no date provided sets tomrrow as due date
+            const tomorrow = new Date()
+            tomorrow.setDate(tomorrow.getDate() + 1)
+            this.#dueDate = tomorrow
+        }
+        else {
+            if (duedate instanceof Date) {
+                this.#dueDate = dueDate
+            }
+            else {
+                // incase date in incorrect
+                const tomorrow = new Date()
+                tomorrow.setDate(tomorrow.getDate() + 1)
+                this.#dueDate = tomorrow
+            }
+        }
+        if (name && typeof name == "string") {
+            this.#name = name
+        }
+        if (description && typeof description == "string") {
+            this.#description = description
+        }
+        if (priority && typeof priority == "string" && Task.validPriorities.includes(priority.toLowerCase())) {
+            this.#priority = priority
+        }
+        else{
+            console.log("priority incorrect setting priority low")
+        }
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    get description() {
+        return this.#description
+    }
+
+    get duedate() {
+        return this.#dueDate
+    }
+
+    get priority() {
+        return this.#priority
+    }
+
+    set priority(priorityLevel){
+        console.log("setting priority")
+        if(typeof priorityLevel !== "string"){
+            console.log('priority cannot be not a string')
+            return false
+        }
+        else if(Task.validPriorities.includes(priorityLevel.toLowerCase()) ){
+            console.log("priority set to: ",priorityLevel)
+            this.#priority = priorityLevel
+        }
+        else{
+            console.log("priority level incorrect")
+            return false
+        }
+    }
+}
